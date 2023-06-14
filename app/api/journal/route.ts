92042452a5ab818d22682f2ea2628c1819e17e15
TomGower <tom.gower@gmail.com>
2023-06-14 12:15:45 -0500

    Basic AI functionality added

--- app/api/journal/route.ts
+++ app/api/journal/route.ts
@@ -1,3 +1,4 @@
+import { analyze } from '@/utils/ai'
 import { getUserByClerkId } from '@/utils/auth'
 import { prisma } from '@/utils/db'
 import { revalidatePath } from 'next/cache'
@@ -14,6 +15,13 @@ export const POST = async () => {
       //   }
       // }
       content: 'Write about your day',
+    },
+  })
+  const { sentimentScore, ...analysis } = (await analyze(entry.content))!
+  await prisma.analysis.create({
+    data: {
+      entryId: entry.id,
+      ...analysis,
     },
   })
 
---
+++ utils/ai.ts
@@ -0,0 +1,60 @@
+import { PromptTemplate } from 'langchain/prompts'
+import { OpenAI } from 'langchain/llms/openai'
+import { StructuredOutputParser } from 'langchain/output_parsers'
+import { z } from 'zod'
+
+const parser = StructuredOutputParser.fromZodSchema(
+  z.object({
+    mood: z
+      .string()
+      .describe('the mood of the person who wrote the journal entry.'),
+    summary: z.string().describe('quick summary of the entire journal entry.'),
+    negative: z
+      .boolean()
+      .describe(
+        'is the journal entry negative? E.g, does it contain negative emotions?'
+      ),
+    subject: z
+      .string()
+      .describe('a basic description of the subject of the journal entry.'),
+    color: z
+      .string()
+      .describe(
+        'a hexadecimal color code that represents the mood of the entry. Example #0101fe for blue representing happiness.'
+      ),
+    sentimentScore: z
+      .number()
+      .describe(
+        'sentiment of the text and rated on a scale from -10 to 10, where -10 is extremely negative, 0 is neutral, and 10 is extremely positive.'
+      ),
+  })
+)
+
+const getPrompt = async (content: string) => {
+  const format_instructions = parser.getFormatInstructions()
+
+  const prompt = new PromptTemplate({
+    template:
+      'Analyze the following journal entry. Follow the instructions and format your response to match the format instructions, no matter what! \n{format_instructions}\n{entry}',
+    inputVariables: ['entry'],
+    partialVariables: { format_instructions },
+  })
+
+  const input = await prompt.format({
+    entry: content,
+  })
+
+  return input
+}
+
+export const analyze = async (content: string) => {
+  const input = await getPrompt(content)
+  const model = new OpenAI({ temperature: 0, modelName: 'gpt-3.5-turbo' })
+  const result = await model.call(input)
+
+  try {
+    return parser.parse(result)
+  } catch (error) {
+    console.log(error)
+  }
+}
